4538dc53daba38607762efdaa70bb94a7d6ebd85
Dan Bejan <dbejan@tremend.ro>
2015-10-14 20:24:44 +0300

    fixed recursive function code cleanup

--- Projects/Dashboard/scripts/main.js
+++ Projects/Dashboard/scripts/main.js
@@ -14,26 +14,25 @@ mainCheckbox.addEventListener("change", function() {
 });
 
 function getValue (value) {
-    // console.log("value is " + value + "\n type: " + typeof value);
     if (typeof value === "string" || typeof value === "number")
     {
-        return value;
+        var container = document.createElement("li");
+        itemDataContainer.appendChild(container);
+        container.textContent = value
+        console.log(value);
     };
     if (typeof value === "array")
     {
         for (var k = 0; k < value.length; k++)
         {
-            // console.log ("property is " + k + "\n value is " + value[k]);
-            return getValue (value[k]);
+             getValue (value[k]);
         }
     };
     if (typeof value === "object")
-    // console.log("value is " + value + "\n type: " + typeof value);
     {
         for (var prop in value)
         {
-            // console.log ("property is " + prop + "\n value is " + value[prop]);
-            return getValue (value[prop]);
+             getValue (value[prop]);
         }
     };
 }
@@ -48,42 +47,7 @@ for (var i = 0; i < items.length; i++)
     var itemContainer = document.createElement("li");
     var itemDataContainer = document.createElement("ul");
 
-    // var idContainer = document.createElement("li");
-    /**
-     * innerHtml - only for html content
-     * innerText - not supported by firefox
-     * textContent - purrrfect
-     */
-    // idContainer.textContent = item.id;
-    // itemDataContainer.appendChild(idContainer);
-
-    // idContainer.textContent = item.name;
-
-    // idContainer.textContent = item.details.description;
-
-    // @TODO
-    // var requirements = ["id", "name", {"details" : "description"}, "filesTypes" : "filename"];
-
-    // for (var j = 0; j < requirements.length; j++) {
-    //     var container = document.createElement("li");
-
-    //     if (typeof requirements[j] === "object") {
-    //         for (var prop in requirements[j]) {
-    //             container.textContent = item[prop][requirements[j][prop]];
-    //         }
-    //     } else {
-    //      container.textContent = item[requirements[j]];
-    //     }
-    // itemDataContainer.appendChild(container);  
-    // }
-
-    for (var j in item)
-    {
-        var container = document.createElement("li");
-        console.log("property is " + j + "\n type " + typeof item[j]);
-        container.textContent = getValue (item[j]);
-        itemDataContainer.appendChild(container);  
-    }
+    getValue(item);
 
     var checkboxContainer = document.createElement("li");
     var checkbox = document.createElement("input");
