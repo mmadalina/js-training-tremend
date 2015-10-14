e46785882aafcaf28eb9dd8134c6ae418ee0b6b7
Dan Bejan <dbejan@tremend.ro>
2015-10-14 23:48:38 +0300

    notes based on Razvan's feedback

---
+++ Projects/Dashboard/scripts/functions.js
@@ -0,0 +1,20 @@
+function getValue (value, property) {
+    if (typeof value === "string" || typeof value === "number")
+    {
+        if (requirements.indexOf(property) != -1)
+        {
+            var label = document.createElement("div");
+            label.className = property;
+    		label.textContent = value;
+            itemDataContainer.appendChild(label);
+            // don't use the global var here;
+        }
+    };
+    if (typeof value === "object")
+    {
+        for (var prop in value)
+        {
+             getValue (value[prop], prop);
+        }
+    };
+}
